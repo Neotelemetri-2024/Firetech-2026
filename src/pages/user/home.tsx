2787ab015bb12177e68baaa55e90c8f1cc68c12c
Rizki Dafa Naldi <rizkidafa0404@gmail.com>
2026-07-23 14:13:55 +0700

    navbarmediapartner2

--- src/pages/user/home.tsx
+++ src/pages/user/home.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 
       <SectionDivider />
 
-      <section id="mediapartner" className="scroll-mt-40">
+      <section id="mediapartner" className="scroll-mt-42">
         <MediaPartner />
       </section>
 
